8c9f219b8660f6bfab4e85a5c5e8cb07331f399a
TarasTikan <tikantaras1@gmail.com>
2023-02-10 21:56:57 +0200

    add dada

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,14 @@
 import { Component } from "react";
 
 export class ImageGallery extends Component {
-    componentDidUpdate (prevState, prevProps) {
+    componentDidUpdate (prevProps, prevState) {
         if(prevProps.query !== this.props.query) {
-            // fetch(
-            //     `https://pixabay.com/api/?q=${this.props.query}&page=1&key=32151990-9b10be0026312dee6c1690c83&image_type=photo&orientation=horizontal&per_page=12`
-            //   )
-            //     .then(res => res.json())
-            //     .then(data => console.log(data));
-            console.log('heloo')
+            fetch(
+                `https://pixabay.com/api/?q=${this.props.query}&page=1&key=32151990-9b10be0026312dee6c1690c83&image_type=photo&orientation=horizontal&per_page=12`
+              )
+                .then(res => res.json())
+                .then(data => console.log(data));
+            // console.log('heloo')
         }
     } 
     render () {
