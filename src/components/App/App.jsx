52d371652b2554caaf9a8e17edeeb006de8f9964
TarasTikan <tikantaras1@gmail.com>
2023-02-19 17:49:37 +0200

    add dada

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,61 +1,105 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Apps } from './App.styled';
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
 import { FetchImg } from 'FetchImg/FetchImg';
-export class App extends Component {
-  state = {
-    page: 1,
-    query: '',
-    items: [],
-    loading: false,
-    visibleLoadMore: false,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ loading: true });
+import { useState, useEffect } from 'react';
+export function App() {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [visibleLoadMore, setVisibleLoadMore] = useState(false);
 
-      FetchImg(query, page)
-        .then(({ hits }) =>
-          this.setState(prevState => ({
-            items: [...prevState.items, ...hits],
-          }))
-        )
-        .finally(() =>
-          this.setState({ loading: false, visibleLoadMore: true })
-        );
+  useEffect(() => {
+    if (query === '') {
+      return;
     }
-  }
+    setLoading(true);
+    FetchImg(query, page)
+      .then(({ hits }) => setItems(prevItems => [...prevItems, ...hits]))
+      .finally(() => {
+        setLoading(false);
+        setVisibleLoadMore(true);
+      });
+  }, [query, page]);
 
-  handleLoadMoreBtn = e => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-      loading: true,
-      visibleLoadMore: false,
-    }));
-  };
-  handleSubmitImg = query => {
-    this.setState({
-      page: 1,
-      query,
-      items: [],
-    });
+  const handleLoadMoreBtn = e => {
+    setPage(prevPage => prevPage + 1);
+    setLoading(true);
+    setVisibleLoadMore(false);
   };
 
-  render() {
-    const { page, query, items, loading, visibleLoadMore } = this.state;
-    return (
-      <Apps>
-        <Searchbar page={page} query={query} onSubmit={this.handleSubmitImg} />
-        {items.length !== 0 && (
-          <ImageGallery items={items} handleModal={this.toggleModal} />
-        )}
-        {loading && <Loader />}
-        {visibleLoadMore && <Button onClick={this.handleLoadMoreBtn} />}
-      </Apps>
-    );
-  }
+  const handleSubmitImg = querys => {
+    if (query === querys) {
+      return;
+    }
+    setPage(1);
+    setQuery(querys);
+    setItems([]);
+  };
+  return (
+    <Apps>
+      <Searchbar page={page} query={query} onSubmit={handleSubmitImg} />
+      {items.length !== 0 && <ImageGallery items={items} />}
+      {loading && <Loader />}
+      {visibleLoadMore && <Button onClick={handleLoadMoreBtn} />}
+    </Apps>
+  );
 }
+// export class App extends Component {
+//   state = {
+//     page: 1,
+//     query: '',
+//     items: [],
+//     loading: false,
+//     visibleLoadMore: false,
+//   };
+//   componentDidUpdate(prevProps, prevState) {
+//     const { query, page } = this.state;
+//     if (prevState.query !== query || prevState.page !== page) {
+//       this.setState({ loading: true });
+
+//       FetchImg(query, page)
+//         .then(({ hits }) =>
+//           this.setState(prevState => ({
+//             items: [...prevState.items, ...hits],
+//           }))
+//         )
+//         .finally(() =>
+//           this.setState({ loading: false, visibleLoadMore: true })
+//         );
+//     }
+//   }
+
+//   handleLoadMoreBtn = e => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//       loading: true,
+//       visibleLoadMore: false,
+//     }));
+//   };
+//   handleSubmitImg = query => {
+//     this.setState({
+//       page: 1,
+//       query,
+//       items: [],
+//     });
+//   };
+
+//   render() {
+//     const { page, query, items, loading, visibleLoadMore } = this.state;
+//     return (
+//       <Apps>
+//         <Searchbar page={page} query={query} onSubmit={this.handleSubmitImg} />
+//         {items.length !== 0 && (
+//           <ImageGallery items={items} handleModal={this.toggleModal} />
+//         )}
+//         {loading && <Loader />}
+//         {visibleLoadMore && <Button onClick={this.handleLoadMoreBtn} />}
+//       </Apps>
+//     );
+//   }
+// }
