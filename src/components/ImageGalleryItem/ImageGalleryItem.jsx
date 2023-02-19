2cdf876e81473e2fcad8e55ab5ab5347d55126a1
TarasTikan <tikantaras1@gmail.com>
2023-02-19 17:52:39 +0200

    add daad

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,37 +1,55 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { GalleryItem, GalleryItemImg } from './ImageGalleryItem.styled';
 import { Modal } from 'components/Modal/Modal';
-
-export class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+import { useState } from 'react';
+export function ImageGalleryItem({ item: { largeImageURL, webformatURL } }) {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModal = e => {
+    setShowModal(!showModal);
   };
-
-  toggleModal = e => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  render() {
-    const { webformatURL, largeImageURL } = this.props.item;
-    return (
-      <>
-        <GalleryItem>
-          <GalleryItemImg
-            src={webformatURL}
-            alt="imageItem"
-            onClick={this.toggleModal}
-          />
-          {this.state.showModal && (
-            <Modal onClose={this.toggleModal} imgModal={largeImageURL} />
-          )}
-        </GalleryItem>
-      </>
-    );
-  }
+  return (
+    <>
+      <GalleryItem>
+        <GalleryItemImg
+          src={webformatURL}
+          alt="imageItem"
+          onClick={toggleModal}
+        />
+        {showModal && <Modal onClose={toggleModal} imgModal={largeImageURL} />}
+      </GalleryItem>
+    </>
+  );
 }
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     showModal: false,
+//   };
+
+//   toggleModal = e => {
+//     this.setState(({ showModal }) => ({
+//       showModal: !showModal,
+//     }));
+//   };
+
+//   render() {
+//     const { webformatURL, largeImageURL } = this.props.item;
+//     return (
+//       <>
+//         <GalleryItem>
+//           <GalleryItemImg
+//             src={webformatURL}
+//             alt="imageItem"
+//             onClick={this.toggleModal}
+//           />
+//           {this.state.showModal && (
+//             <Modal onClose={this.toggleModal} imgModal={largeImageURL} />
+//           )}
+//         </GalleryItem>
+//       </>
+//     );
+//   }
+// }
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.shape({
