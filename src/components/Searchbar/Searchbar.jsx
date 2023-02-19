8b12372d5ed3b30ee2e09a289057c4588ec01beb
TarasTikan <tikantaras1@gmail.com>
2023-02-19 17:58:00 +0200

    add dada

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import { Component } from 'react';
 import {
   Search,
   SearchForm,
@@ -7,38 +7,67 @@ import {
   SearchButtonLabel,
   SearchInput,
 } from './Searchbar.styled';
-export class Searchbar extends Component {
-  state = {
-    query: '',
-  };
-
-  handleSubmit = e => {
+import { useState } from 'react';
+export function Searchbar({ onSubmit }) {
+  const [query, setQuery] = useState('');
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state.query);
-  };
-  handleQueryChange = e => {
-    this.setState({ query: e.currentTarget.value.toLowerCase() });
+    onSubmit(query);
   };
 
-  render() {
-    return (
-      <Search>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchButton type="submit">
-            <SearchButtonLabel>Search</SearchButtonLabel>
-          </SearchButton>
+  const handleQueryChange = e => {
+    setQuery(e.currentTarget.value.toLowerCase());
+  };
 
-          <SearchInput
-            onChange={this.handleQueryChange}
-            type="text"
-            name="query"
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </Search>
-    );
-  }
+  return (
+    <Search>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchButton type="submit">
+          <SearchButtonLabel>Search</SearchButtonLabel>
+        </SearchButton>
+
+        <SearchInput
+          onChange={handleQueryChange}
+          type="text"
+          name="query"
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </Search>
+  );
 }
+// export class Searchbar extends Component {
+//   state = {
+//     query: '',
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit(this.state.query);
+//   };
+//   handleQueryChange = e => {
+//     this.setState({ query: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   render() {
+//     return (
+//       <Search>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <SearchButton type="submit">
+//             <SearchButtonLabel>Search</SearchButtonLabel>
+//           </SearchButton>
+
+//           <SearchInput
+//             onChange={this.handleQueryChange}
+//             type="text"
+//             name="query"
+//             placeholder="Search images and photos"
+//           />
+//         </SearchForm>
+//       </Search>
+//     );
+//   }
+// }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
