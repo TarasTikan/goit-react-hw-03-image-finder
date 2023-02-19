68a9fe691959d88c338a9711b0aac6e5926a59b6
TarasTikan <tikantaras1@gmail.com>
2023-02-19 17:54:44 +0200

    add daad

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,69 @@
 import { ModalImg, Overlay } from './Modal.styled';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+// import { Component } from 'react';
 import { createPortal } from 'react-dom';
+import { useState, useEffect } from 'react';
 const ModalRoot = document.querySelector('#ModalRoot');
+export function Modal({ imgModal, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', keyDown);
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.keyDown);
-  }
-
-  keyDown = e => {
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  });
+  const keyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  onOverlayClose = e => {
+  const onOverlayClose = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-
-  render() {
-    return createPortal(
-      <Overlay onClick={this.onOverlayClose}>
-        <ModalImg>
-          <img src={this.props.imgModal} alt="pictureModal" />
-        </ModalImg>
-      </Overlay>,
-      ModalRoot
-    );
-  }
+  return createPortal(
+    <Overlay onClick={onOverlayClose}>
+      <ModalImg>
+        <img src={imgModal} alt="pictureModal" />
+      </ModalImg>
+    </Overlay>,
+    ModalRoot
+  );
 }
+// export class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.keyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.keyDown);
+//   }
+
+//   keyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   onOverlayClose = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.onOverlayClose}>
+//         <ModalImg>
+//           <img src={this.props.imgModal} alt="pictureModal" />
+//         </ModalImg>
+//       </Overlay>,
+//       ModalRoot
+//     );
+//   }
+// }
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
